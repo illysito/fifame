77cc744c7cfd71ffd270896ee285ce8f4307869f
illysito <illyalukyanovdesign@gmail.com>
2024-08-08 15:09:35 +0100

    Update pin.js

--- src/features/pin.js
+++ src/features/pin.js
@@ -12,7 +12,20 @@ function pin() {
       trigger: '.map-container',
       start: 'top top',
       endTrigger: '.cards-container',
-      end: 'bottom 99%',
+      end: 'bottom bottom',
+      markers: false,
+    },
+  })
+
+  gsap.to('.who-hero-image-wrapper', {
+    scrollTrigger: {
+      pin: true,
+      pinSpacing: true,
+      anticipatepin: true,
+      trigger: '.who-hero-image-wrapper',
+      start: 'bottom bottom',
+      endTrigger: '.who-type-wrapper',
+      end: 'bottom bottom',
       markers: false,
     },
   })
